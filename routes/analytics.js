7b34adb1a516346309c3d2fe90a5ba920582a9ca
Marcos <mg5860606@gmail.com>
2026-03-19 20:29:51 -0300

    primeiro commit

--- routes/analytics.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Rota placeholder para analytics
-router.get('/', (req, res) => {
-  res.json({ message: 'Analytics API' });
-});
-
-module.exports = router;
